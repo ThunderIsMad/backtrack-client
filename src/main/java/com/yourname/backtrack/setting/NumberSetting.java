8ebf4afff8543becca0b0d93bec7e61fe50b5a21
ThunderIsMad <93434168+ThunderIsMad@users.noreply.github.com>
2026-06-22 09:45:27 +0100

    Update NumberSetting.java

--- src/main/java/com/yourname/backtrack/setting/NumberSetting.java
+++ src/main/java/com/yourname/backtrack/setting/NumberSetting.java
@@ -1,60 +1,28 @@
-package com.yourname.backtrack.setting;
+package com.yourname.backtrack.setting
 
-import setting.SettingGroup;
+import setting.SettingGroup
 
-public class NumberSetting extends Setting {
+open class NumberSetting(
+    name: String,
+    defaultValue: Double,
+    val min: Double,
+    val max: Double,
+    val increment: Double,
+    group: SettingGroup = SettingGroup.MAIN
+) : Setting(name, group) {
 
-    private double value;
-    private final double min;
-    private final double max;
-    private final double increment;
-
-    public NumberSetting(String name, double defaultValue, double min, double max, double increment) {
-        this(name, defaultValue, min, max, increment, SettingGroup.MAIN);
-    }
-
-    public NumberSetting(String name, double defaultValue, double min, double max, double increment, SettingGroup group) {
-        super(name, group);
-        this.value = defaultValue;
-        this.min = min;
-        this.max = max;
-        this.increment = increment;
-    }
-
-    public double getValue() {
-        return value;
-    }
-
-    public void setValue(double value) {
-        if (value < min) {
-            value = min;
+    var value: Double = defaultValue
+        protected set(v) {
+            field = v.coerceIn(min, max)
         }
 
-        if (value > max) {
-            value = max;
-        }
+    fun increase() { value += increment }
+    fun decrease() { value -= increment }
 
-        this.value = value;
-    }
-
-    public void increase() {
-        setValue(value + increment);
-    }
-
-    public void decrease() {
-        setValue(value - increment);
-    }
-
-    public double getMin() {
-        return min;
-    }
-
-    public double getMax() {
-        return max;
-    }
-
-    public double getIncrement() {
-        return increment;
-    }
+    // Java compatibility
+    fun getValue(): Double = value
+    fun setValue(v: Double) { value = v }
+    fun getMin(): Double = min
+    fun getMax(): Double = max
+    fun getIncrement(): Double = increment
 }
-
