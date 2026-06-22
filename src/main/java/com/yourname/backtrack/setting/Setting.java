5850cdaad3fb7b171e352a7268f0bb4f14597051
ThunderIsMad <93434168+ThunderIsMad@users.noreply.github.com>
2026-06-22 09:43:38 +0100

    Update Setting.java

--- src/main/java/com/yourname/backtrack/setting/Setting.java
+++ src/main/java/com/yourname/backtrack/setting/Setting.java
@@ -1,26 +1,8 @@
-package com.yourname.backtrack.setting;
+package com.yourname.backtrack.setting
 
-import setting.SettingGroup;
+import setting.SettingGroup
 
-public abstract class Setting {
-
-    private final String name;
-    private final SettingGroup group;
-
-    public Setting(String name) {
-        this(name, SettingGroup.MAIN);
-    }
-
-    public Setting(String name, SettingGroup group) {
-        this.name = name;
-        this.group = group == null ? SettingGroup.MAIN : group;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public SettingGroup getGroup() {
-        return group;
-    }
-}
+abstract class Setting(
+    val name: String,
+    val group: SettingGroup = SettingGroup.MAIN
+)
